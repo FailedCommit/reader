49afb144a25a6eac421bf18c4a0d35c8df4331f1
Mohit Kumar <mkumar@tekion.com>
2021-03-14 19:22:41 +0530

    Implements loading cache.

--- mongosupport/src/main/java/com/maat/mongo/serverconfig/beans/MaatConfigProperties.java
+++ mongosupport/src/main/java/com/maat/mongo/serverconfig/beans/MaatConfigProperties.java
@@ -3,20 +3,22 @@ package com.maat.mongo.serverconfig.beans;
 //TODO: copied from config-service. This will be required in all the support libraries.
 // Move to sdk-core library to avoid duplication
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Data
 @Component
 @ConfigurationProperties("maat.config")
 public class MaatConfigProperties {
-    private long configRefreshTimeInMinutes = 1L;
-    private long readTimeOutInSeconds = 10L;
-    private long connectTimeOutInSeconds = 10L;
+    private long configRefreshTime = 60L;
+    private long readTimeOut = 10L;
+    private long connectTimeOut = 10L;
     private int minIdleConnections = 5;
     private int maxIdleConnections = 60;
-    private int stayAliveTimeInSeconds = 90;
+    private int stayAliveTime = 90;
     private String poolName = "maat-default-pool";
     private int corePoolSize = 10;
     private int maxPoolSize = 120;
-    private long configExpireTimeInMinutes = 600L;
+    private long configExpiryTime = 5L;
 }
--- mongosupport/src/main/java/com/maat/mongo/serverconfig/service/ServerConfigServiceImpl.java
+++ mongosupport/src/main/java/com/maat/mongo/serverconfig/service/ServerConfigServiceImpl.java
@@ -1,6 +1,10 @@
 package com.maat.mongo.serverconfig.service;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListenableFutureTask;
 import com.maat.mongo.serverconfig.beans.MaatConfigProperties;
 import com.maat.mongo.serverconfig.beans.ServerConfig;
 import lombok.AllArgsConstructor;
@@ -11,32 +15,36 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import javax.annotation.PostConstruct;
 import java.util.Collection;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 
 import static java.util.Objects.nonNull;
+import static java.util.concurrent.TimeUnit.MINUTES;
 
 @Service
 @Slf4j
-@RequiredArgsConstructor
 public class ServerConfigServiceImpl implements ServerConfigService {
   private final MaatConfigProperties maatConfigProperties;
-  private LoadingCache<ConfigCacheKey, ServerConfig> configCache;
+  private final ServerConfigService configService;
+  private LoadingCache<ConfigCacheWrapper, ServerConfig> configCache;
   private final ExecutorService executor;
 
-  public ServiceConfigServiceImpl(
+  public ServerConfigServiceImpl(
       MaatConfigProperties maatConfigProperties,
+      ServerConfigService configService,
       @Qualifier("configExecutor") ExecutorService executor) {
     this.maatConfigProperties = maatConfigProperties;
+    this.configService = configService;
     this.executor = executor;
   }
 
   @Data
   @AllArgsConstructor
   @Builder
-  private static class ConfigCacheKey {
+  private static class ConfigCacheWrapper {
     private String type;
     private String moduleName;
   }
@@ -47,17 +55,51 @@ public class ServerConfigServiceImpl implements ServerConfigService {
     setConfigClientProperties();
   }
 
+  private LoadingCache<ConfigCacheWrapper, ServerConfig> createLoadingCache() {
+    final long configRefreshTime = maatConfigProperties.getConfigRefreshTime();
+    final long configExpiryTime = maatConfigProperties.getConfigExpiryTime();
+    return CacheBuilder.newBuilder()
+        .concurrencyLevel(10)
+        .maximumSize(500)
+        .refreshAfterWrite(configRefreshTime, MINUTES)
+        .expireAfterWrite(configExpiryTime, MINUTES)
+        .build(
+            new CacheLoader<ConfigCacheWrapper, ServerConfig>() {
+              @Override
+              @ParametersAreNonnullByDefault
+              public ServerConfig load(ConfigCacheWrapper keyWrapper) throws Exception {
+                return fetchServerConfigByKey(keyWrapper);
+              }
+
+              @Override
+              public ListenableFuture<ServerConfig> reload(
+                  ConfigCacheWrapper keyWrapper, ServerConfig oldValue) throws Exception {
+                ListenableFutureTask<ServerConfig> task =
+                    ListenableFutureTask.create(() -> fetchServerConfigByKey(keyWrapper));
+                executor.execute(task);
+                return task;
+              }
+            });
+  }
+
+  private void setConfigClientProperties() {
+    // TODO: Implement me
+  }
+
+  private ServerConfig fetchServerConfigByKey(ConfigCacheWrapper wrapper) {
+    // TODO: Create a ConfigService client and use use that instead.
+    return configService.findServerConfig(wrapper.getType(), wrapper.getModuleName());
+  }
+
   @Override
   public ServerConfig findServerConfig(String type, String moduleName) {
-    final ConfigCacheKey key =
-            ConfigCacheKey.builder()
-                    .type(type)
-                    .moduleName(moduleName)
-                    .build();
+    final ConfigCacheWrapper key =
+        ConfigCacheWrapper.builder().type(type).moduleName(moduleName).build();
     try {
       return configCache.get(key);
     } catch (ExecutionException e) {
-      e.printStackTrace();
+      log.error("Failed to load config from the cache, Exception: {}", e);
+      throw new RuntimeException(e);
     }
   }
 
@@ -68,7 +110,7 @@ public class ServerConfigServiceImpl implements ServerConfigService {
       configCache.invalidateAll();
       outcome = true;
     } else if (nonNull(invalidationTypes) && !invalidationTypes.isEmpty()) {
-        configCache.invalidateAll(invalidationTypes);
+      configCache.invalidateAll(invalidationTypes);
     }
     return outcome;
   }
